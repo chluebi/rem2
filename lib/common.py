08e76ec8c359d29a1de62d29c7cf966d81719985
Lu <luis.maltsis@mng.ch>
2020-01-04 18:49:06 +0100

    added rudimentary list command

--- lib/common.py
+++ lib/common.py
@@ -4,3 +4,13 @@ def parse_config(name):
 	with open(f'configs/{name}_config.json', 'r+') as f:
 		data = json.load(f)
 	return data
+
+
+def get_message_link(guild_id, channel_id, message_id, receiver_id):
+	if guild_id == 0:
+		created_message_link = f'https://discordapp.com/channels/@me/{receiver_id}/{message_id}'
+	elif message_id == 0
+		created_message_link = 'Timer created in Web.'
+	else:
+		created_message_link = f'https://discordapp.com/channels/{guild_id}/{channel_id}/{message_id}'
+	return created_message_link
