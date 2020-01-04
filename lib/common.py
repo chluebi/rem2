fd88372fbb241ea552dc8f2dd89fb76022ef8f3d
Lu <luis.maltsis@mng.ch>
2020-01-04 18:49:06 +0100

    added rudimentary list command

--- lib/common.py
+++ lib/common.py
@@ -4,3 +4,11 @@ def parse_config(name):
 	with open(f'configs/{name}_config.json', 'r+') as f:
 		data = json.load(f)
 	return data
+
+
+def get_message_link(guild_id, channel_id, message_id, receiver_id):
+	if guild_id == 0:
+		created_message_link = f'https://discordapp.com/channels/@me/{receiver_id}/{message_id}'
+	else:
+		created_message_link = f'https://discordapp.com/channels/{guild_id}/{channel_id}/{message_id}'
+	return created_message_link
