44f1f199435f38c36f0366ec9151c4df8fb05a84
Lu <luis.maltsis@mng.ch>
2020-01-04 18:55:30 +0100

    remove head

--- lib/common.py
+++ lib/common.py
@@ -9,11 +9,8 @@ def parse_config(name):
 def get_message_link(guild_id, channel_id, message_id, receiver_id):
 	if guild_id == 0:
 		created_message_link = f'https://discordapp.com/channels/@me/{receiver_id}/{message_id}'
-<<<<<<< HEAD
 	elif message_id == 0
 		created_message_link = 'Timer created in Web.'
-=======
->>>>>>> development
 	else:
 		created_message_link = f'https://discordapp.com/channels/{guild_id}/{channel_id}/{message_id}'
 	return created_message_link
